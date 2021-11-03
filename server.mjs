3015faf52712687792c1e08e4c6923d98a381790
Farrukh Aslam <47663235+lvlFarrukh@users.noreply.github.com>
2021-11-03 19:15:35 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import express from 'express'
 const port = process.env.PORT || 3000;
 
 const app = express();
-
+console.log("------------->",process.env.MDB);
 app.get('/', (req, res) => {
     res.send("Pakistan Zindabad");
 })
